e9a80d820dbe8793f06a2e27db68a4a15fa229f2
isabu77 <isabu77@gmail.com>
2019-05-07 11:56:00 +0200

    JS 1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+var joueur = 1;
+var nbColonnes = 5;
+var nbLignes = 5;
+var jeu = true;
+var texte = "";
+var plateau = [];
+
+for (var i = 0; i < nbLignes; i++) {
+	plateau[i] = [];
+}
+
 function newGame(){
 	console.log("newGame");
 }
+
