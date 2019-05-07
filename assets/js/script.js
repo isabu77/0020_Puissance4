bf512908efef82d328c8a724f6481b8890a0c040
isabu77 <isabu77@gmail.com>
2019-05-07 16:03:55 +0200

    JS 4 dur dur

--- assets/js/script.js
+++ assets/js/script.js
@@ -41,7 +41,7 @@ function creerTableau(){
     for (i = 0; i < nbLignes; i++) {
         texte += '<tr>';
         for (j = 0; j < nbColonnes; j++) {
-            texte += '<td onclick="detecteClick('+j+')" id="'+i+'-'+j+'">';
+            texte += '<td onclick="detectClick(' + j + ')" id="' + i + '-' + j + '">';
             if(plateau[i][j] == 1){
                 texte += '<div class="joueur1">';
             }else if(plateau[i][j] == 2){
@@ -52,6 +52,53 @@ function creerTableau(){
         texte += '</tr>';
     }
     texte += '</table>';
-    tableau = document.getElementById('puissance4');
-    tableau.innerHTML = texte;
+    document.getElementById('puissance4').innerHTML = texte;
+}
+
+function detectClick(j){
+	if(verifPosition(j) && jeu){
+		var ligneEnCours = poseJeton(j); /* numero de la ligne en cours */
+		var verifEnd = puissanceQuatre(ligneEnCours, j, 0, 0);
+		if(verifEnd){
+			jeu = false;
+			afficheTextAnnonce("Le joueur " + nomDuJoueur(joueur) + " a gagné la partie.");
+		}else{
+			if (joueur == 1){
+				joueur = 2;
+			}else{
+				joueur = 1;
+			}
+			afficheTextAnnonce("c'est au tour du joueur " + nomDuJoueur(joueur));
+		}
+	}
+}
+
+function verifPosition(j){
+	if(plateau[0][j] == 0){
+		return true;
+	}
+	else{
+		return false;
+	}
+}
+
+/* no de la ligne disponible où le jeton peut etre posé */
+function poseJeton(j){
+	for (var i = nbLignes-1; i >= 0; i--) {
+		if(plateau[i][j] == 0){
+			plateau[i][j] = joueur;
+			refreshTableau(i, j, joueur);
+			return (i);
+		}
+	}
+}
+
+function refreshTableau(x, y, i){
+	document.getElementById(x + '-' + y).innerHTML = "<div class='joueur" + i + "'></div>";
+
+}
+
+function puissanceQuatre(curLigne, curCol, l, c){
+	console.log("valeur : " + curLigne + " " + curCol + " / increment " + l + " " + c);
+	return false;
 }
