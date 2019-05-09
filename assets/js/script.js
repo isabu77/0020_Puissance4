dd7af85ca216ab9dffa4f1ff3e9296e1b5205e5d
isabu77 <isabu77@gmail.com>
2019-05-09 12:19:25 +0200

    fonction récursive de détection du gagnant

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var nbColonnes = 5;
 var nbLignes = 5;
 var jeu = true;
 var texte = "";
-var plateau = [];
+var plateau = [];  // ou new Array();
 
 for (var i = 0; i < nbLignes; i++) {
 	plateau[i] = [];
@@ -12,14 +12,15 @@ for (var i = 0; i < nbLignes; i++) {
 newGame();
 
 function newGame(){
-	for (var i = 0; i < nbLignes; i++) {
-		for (var j = 0; j < nbColonnes; j++) {
-			plateau[i][j] = 0;
+	// this : "celui du fichier", cad du contexte courant
+	for (var i = 0; i < this.nbLignes; i++) {
+		for (var j = 0; j < this.nbColonnes; j++) {
+			this.plateau[i][j] = 0;
 		}
 	}
-	joueur = 1;
-	afficheTextAnnonce("Le jeu commence ! c'est au tour du joueur " + nomDuJoueur(joueur));
-	jeu = true;
+	this.joueur = 1;
+	afficheTextAnnonce("Le jeu commence ! c'est au tour du joueur " + nomDuJoueur(this.joueur));
+	this.jeu = true;
 	creerTableau();
 }
 
@@ -37,44 +38,50 @@ function nomDuJoueur(numJoueur){
 
 
 function creerTableau(){
-    texte = '<table>';
-    for (i = 0; i < nbLignes; i++) {
-        texte += '<tr>';
-        for (j = 0; j < nbColonnes; j++) {
-            texte += '<td onclick="detectClick(' + j + ')" id="' + i + '-' + j + '">';
-            if(plateau[i][j] == 1){
-                texte += '<div class="joueur1">';
-            }else if(plateau[i][j] == 2){
-                texte += '<div class="joueur2">';
+    this.texte = '<table>';
+    for (i = 0; i < this.nbLignes; i++) {
+        this.texte += '<tr>';
+        for (j = 0; j < this.nbColonnes; j++) {
+            this.texte += '<td onclick="detectClic(' + j + ')" id="' + i + '-' + j + '">';
+            if(this.plateau[i][j] == 1){
+                this.texte += '<div class="joueur1">';
+            }else if(this.plateau[i][j] == 2){
+                this.texte += '<div class="joueur2">';
             }
-            texte += '</td>';
+            this.texte += '</td>';
         }
-        texte += '</tr>';
+        this.texte += '</tr>';
     }
-    texte += '</table>';
-    document.getElementById('puissance4').innerHTML = texte;
+    this.texte += '</table>';
+    document.getElementById('puissance4').innerHTML = this.texte;
 }
 
-function detectClick(j){
-	if(verifPosition(j) && jeu){
+function detectClic(j){
+	// s'il reste une case de libre dans la colonne et si e jeu est en cours
+	if(verifPosition(j) && this.jeu){
 		var ligneEnCours = poseJeton(j); /* numero de la ligne en cours */
+		
+		// la vérif si vainqueur
 		var verifEnd = puissanceQuatre(ligneEnCours, j, 0, 0);
+
 		if(verifEnd){
-			jeu = false;
-			afficheTextAnnonce("Le joueur " + nomDuJoueur(joueur) + " a gagné la partie.");
+			this.jeu = false;
+			afficheTextAnnonce("Le joueur " + nomDuJoueur(this.joueur) + " a gagné la partie.");
 		}else{
-			if (joueur == 1){
-				joueur = 2;
+			this.joueur == 1 ? this.joueur = 2 : this.joueur = 1;
+/*			if (this.joueur == 1){
+				this.joueur = 2;
 			}else{
-				joueur = 1;
+				this.joueur = 1;
 			}
-			afficheTextAnnonce("c'est au tour du joueur " + nomDuJoueur(joueur));
+*/			
+			afficheTextAnnonce("c'est au tour du joueur " + nomDuJoueur(this.joueur));
 		}
 	}
 }
 
 function verifPosition(j){
-	if(plateau[0][j] == 0){
+	if(this.plateau[0][j] == 0){
 		return true;
 	}
 	else{
@@ -84,10 +91,10 @@ function verifPosition(j){
 
 /* no de la ligne disponible où le jeton peut etre posé */
 function poseJeton(j){
-	for (var i = nbLignes-1; i >= 0; i--) {
-		if(plateau[i][j] == 0){
-			plateau[i][j] = joueur;
-			refreshTableau(i, j, joueur);
+	for (var i = this.nbLignes-1; i >= 0; i--) {
+		if(this.plateau[i][j] == 0){
+			this.plateau[i][j] = this.joueur;
+			refreshTableau(i, j, this.joueur);
 			return (i);
 		}
 	}
@@ -95,10 +102,52 @@ function poseJeton(j){
 
 function refreshTableau(x, y, i){
 	document.getElementById(x + '-' + y).innerHTML = "<div class='joueur" + i + "'></div>";
-
 }
 
-function puissanceQuatre(curLigne, curCol, l, c){
-	console.log("valeur : " + curLigne + " " + curCol + " / increment " + l + " " + c);
-	return false;
-}
+function puissanceQuatre(lig, col, l, c){
+	// condition primaire de la récursivité
+	if (c == 0 && l == 0){
+		console.log("initial valeurs : " + lig + " " + col + " / Incrément " + l + " " + c);
+
+		// ce 1er appel lance les appels récursifs
+		// -1 pour decaler d'une colonne à gauche ou en haut (et ne pas revenir dans cette condition 0 0) 
+		// 1 pour decaler d'une colonne à droite ou en bas (et ne pas revenir dans cette condition 0 0) 
+		var va = 1 + puissanceQuatre(lig, col-1, 0, -1) + puissanceQuatre(lig, col+1, 0, 1); 
+		
+		// vertical
+		var vb = 1 + puissanceQuatre(lig-1, col, -1, 0) + puissanceQuatre(lig+1, col, 1, 0);
+
+		// diagonale qui descend vers la gauche
+		var vc = 1 + puissanceQuatre(lig-1, col-1, -1, -1) + puissanceQuatre(lig+1, col+1, 1, 1);
+
+		// diagonale qui descend vers la droite
+		var vd = 1 + puissanceQuatre(lig-1, col+1, -1, 1) + puissanceQuatre(lig+1, col-1, 1, -1);
+
+		console.log(va,vb,vc,vd);
+		if(va == 4 || vb == 4 || vc == 4 || vd == 4){
+			return true;
+		}
+		else{
+			return false;
+		}		
+	}
+
+	// condition terminale
+	if (lig < this.nbLignes && lig >= 0 && col < this.nbColonnes && col >= 0){
+		console.log("récursive valeurs : " + lig + " " + col + " / Incrément " + l + " " + c);
+		if (this.plateau[lig][col] == this.joueur){
+			//console.log("ok");
+			// boucle récursive avec le décalage d'entrée (-1 ou +1) pour voir les cases suivantes
+			return 1 + puissanceQuatre(lig + l, col + c, l, c);
+		}
+		else{
+			//console.log("pas ok");
+			return 0;
+		}
+	}
+
+	return 0;
+}	
+
+
+
